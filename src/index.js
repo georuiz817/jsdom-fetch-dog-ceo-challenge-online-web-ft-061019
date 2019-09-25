f30f318000bd7c76d06d6119240388237d455f88
George <georuiz817@gmail.com>
2019-09-25 12:56:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+function fetchDogs() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(json => renderDogs(json))
+  }
+
+
+
